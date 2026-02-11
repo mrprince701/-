f70e63defb4636aab81de91c5f4720f3cfd0a802
mrprince701 <hussainbillal248@gmail.com>
2026-02-11 23:09:41 +0100

    Update Mentionadmin.js

--- Script/commands/Mentionadmin.js
+++ Script/commands/Mentionadmin.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "adminmention",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "SHAHADAT SAHU ",
+ credits: "𓆩𝐌𝐑ܔ🅟︎𝐑𝐈𝐍𝐂𝐄𓆪 ",
  description: "Bot will reply when someone tags any of the admins",
  commandCategory: "Other",
  usages: "@",
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 module.exports.handleEvent = function({ api, event }) {
- const adminIDs = ["61586663640808", "61586663640809", "100044713412032"].map(String); //update your UID✅
+ const adminIDs = ["100071068076153", "", ""].map(String); //update your UID✅
  
  if (adminIDs.includes(String(event.senderID))) return;
 
